08d72c171f6cc65aea7d7da8fb4c252d25546963
Jake Johnson <jtj2197@gmail.com>
2025-02-25 23:11:29 -0600

    lets see if this fixes that hydration issue

--- dorado-exchange-frontend/tailwind.config.js
+++ dorado-exchange-frontend/tailwind.config.js
@@ -1,4 +1,4 @@
-import {heroui} from "@heroui/theme"
+import {colors, heroui} from "@heroui/theme"
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -8,15 +8,18 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
-  // theme: {
-  //   extend: {
-  //     fontFamily: {
-  //       sans: ["var(--font-sans)"],
-  //       mono: ["var(--font-mono)"],
-  //     },
-  //   },
-  // },
-  // darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ffb400",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        mono: ["var(--font-mono)"],
+      },
+    },
+  },
+  darkMode: "class",
   plugins: [
     heroui({
       "themes": {
