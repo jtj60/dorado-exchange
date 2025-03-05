bcf5c9e3747cea558f28d842d3851ba32f855c20
Jake Johnson <jtj2197@gmail.com>
2025-03-05 17:32:36 -0600

    responsive navigation shell

--- dorado-exchange-frontend/app/layout.tsx
+++ dorado-exchange-frontend/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/custom/navbar";
+import Shell from "@/components/custom/shell";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-         <Navbar />
+         <Shell />
         {children}
       </body>
     </html>
