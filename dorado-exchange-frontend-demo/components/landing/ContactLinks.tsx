a7e5f04b41d0bb46f862e280cc56f5948f85fb82
Jake Johnson <jtj2197@gmail.com>
2025-03-04 23:19:42 -0600

    update links and favicon

--- dorado-exchange-frontend-demo/components/landing/ContactLinks.tsx
+++ dorado-exchange-frontend-demo/components/landing/ContactLinks.tsx
@@ -7,7 +7,7 @@ export default function ContactLinks() {
       <Chip className="bg-transparent border-0 text-white text-md" variant="faded">
         <div className="flex items-center gap-1">
           <Phone className="w-6 h-6 text-primary" />
-          <a href="tel:+1234567890" className="text-white hover:underline block sm:hidden">
+          <a href="tel:+8172034786" className="text-white hover:underline block sm:hidden">
             (817) 203 - 4786
           </a>
           <p className="hidden sm:block">(817) 203 - 4786</p>
@@ -17,7 +17,7 @@ export default function ContactLinks() {
       <Chip className="bg-transparent border-0 text-white text-md" variant="faded">
         <div className="flex items-center gap-1">
           <Envelope className="w-6 h-6 text-primary-500" />
-          <a href="mailto:dorado-metals@outlook.com" className="text-white hover:underline block sm:hidden">
+          <a href="mailto:exchange@doradometals.com" className="text-white hover:underline block sm:hidden">
             exchange@doradometals.com
           </a>
           <p className="hidden sm:block">exchange@doradometals.com</p>
