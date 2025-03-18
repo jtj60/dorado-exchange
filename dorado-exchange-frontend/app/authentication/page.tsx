34c9271ed0edfb46f196bf0af6183f04b8df5640
Jake Johnson <jtj2197@gmail.com>
2025-03-18 14:58:40 -0500

    whoopsie

--- dorado-exchange-frontend/app/authentication/page.tsx
+++ dorado-exchange-frontend/app/authentication/page.tsx
@@ -1,12 +1,14 @@
-'use client';
+'use client'
 
-import { SignInAndUpTabs } from "@/components/custom/auth/SignInAndUpTabs";
+import { SignInAndUpTabs } from '@/components/custom/auth/SignInAndUpTabs'
+import { Suspense } from 'react'
 
 export default function Page() {
-
   return (
     <div className="flex flex-col items-center">
-      <SignInAndUpTabs />
+      <Suspense fallback={<p>Loading...</p>}>
+        <SignInAndUpTabs />
+      </Suspense>
     </div>
-  );
+  )
 }
