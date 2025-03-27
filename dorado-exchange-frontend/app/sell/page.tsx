1484a9dae6111d0f1108582bee3b65599df3f7f0
Jake Johnson <jtj2197@gmail.com>
2025-03-27 14:18:59 -0500

    add suspense for build

--- dorado-exchange-frontend/app/sell/page.tsx
+++ dorado-exchange-frontend/app/sell/page.tsx
@@ -1,11 +1,14 @@
-'use client';
+'use client'
 
-import { SellTabs } from "@/components/custom/sell-tabs/sellTabs";
+import { SellTabs } from '@/components/custom/sell-tabs/sellTabs'
+import { Suspense } from 'react'
 
 export default function Page() {
   return (
-      <div className="flex flex-col items-center gap-3">
+    <div className="flex flex-col items-center gap-3">
+      <Suspense fallback={<p>Loading...</p>}>
         <SellTabs />
-      </div>
+      </Suspense>
+    </div>
   )
 }
