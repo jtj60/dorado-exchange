0498bbcef92c3f554e080f71a979a4f1ae4cf420
Jake Johnson <jtj2197@gmail.com>
2025-05-20 16:16:20 -0500

    update db i guess?

--- dorado-exchange-api/db.js
+++ dorado-exchange-api/db.js
@@ -4,10 +4,9 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-  max: 50,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 5000,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
