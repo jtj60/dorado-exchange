879043a2a7768367c3a29113f6610b689563c1a5
Jake Johnson <jtj2197@gmail.com>
2025-05-20 16:04:23 -0500

    testing pool connections

--- dorado-exchange-api/services/scheduler.js
+++ dorado-exchange-api/services/scheduler.js
@@ -5,24 +5,24 @@ const {
 } = require("../controllers/admin/adminPurchaseOrdersController");
 
 function setupScheduler() {
-  try {
-    updateSpotPrices();
-  } catch (err) {
-    console.error("Startup: Failed to update spot prices", err);
-  }
+  // try {
+  //   updateSpotPrices();
+  // } catch (err) {
+  //   console.error("Startup: Failed to update spot prices", err);
+  // }
 
-  try {
-    expireStaleOffers();
-  } catch (err) {
-    console.error("Startup: Failed to expire offers", err);
-  }
+  // try {
+  //   expireStaleOffers();
+  // } catch (err) {
+  //   console.error("Startup: Failed to expire offers", err);
+  // }
 
-  cron.schedule(process.env.SPOT_UPDATE_SCHEDULE, async () => {
-    await updateSpotPrices();
-  });
+  // cron.schedule(process.env.SPOT_UPDATE_SCHEDULE, async () => {
+  //   await updateSpotPrices();
+  // });
 
-  cron.schedule(process.env.STALE_OFFERS_UPDATE_SCHEDULE, async () => {
-    await expireStaleOffers();
-  });
+  // cron.schedule(process.env.STALE_OFFERS_UPDATE_SCHEDULE, async () => {
+  //   await expireStaleOffers();
+  // });
 }
 module.exports = { setupScheduler };
