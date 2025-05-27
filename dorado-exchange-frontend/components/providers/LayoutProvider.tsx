031aecbb65733305451ff8a9736b9821ece5cce1
Jake Johnson <jtj2197@gmail.com>
2025-05-27 15:56:48 -0500

    add breadcrumb nav

--- dorado-exchange-frontend/components/providers/LayoutProvider.tsx
+++ dorado-exchange-frontend/components/providers/LayoutProvider.tsx
@@ -7,6 +7,16 @@ import { useGetSession, useStopImpersonation } from '@/lib/queries/useAuth'
 import ShellSkeleton from '../skeletons/ShellSkeleton'
 import Footer from '../custom/nav/footer'
 import { Button } from '../ui/button'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '../ui/breadcrumb'
+import React from 'react'
+import Link from 'next/link'
 
 export default function LayoutProvider({ children }: { children: React.ReactNode }) {
   const pathname = usePathname()
@@ -51,6 +61,8 @@ export default function LayoutProvider({ children }: { children: React.ReactNode
             </div>
           </div>
         )}
+        {pathname !== '/' && <BreadcrumbNav />}
+
         <div className="flex flex-col relative flex-grow">
           {showMobileCarousel && <MobileProductCarousel />}
           {children}
@@ -60,5 +72,48 @@ export default function LayoutProvider({ children }: { children: React.ReactNode
         </div>
       </div>
     </>
+  )
+}
+
+function BreadcrumbNav() {
+  const pathname = usePathname()
+  const segments = pathname.split('/').filter(Boolean)
+
+  const formatSegment = (segment: string) =>
+    segment.replace(/-/g, ' ').replace(/\b\w/g, (char) => char.toUpperCase())
+
+  return (
+    <div className="hidden lg:flex pt-4 px-20 pb-0">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+
+          {segments.map((segment, i) => {
+            const href = '/' + segments.slice(0, i + 1).join('/')
+            const isLast = i === segments.length - 1
+            const label = formatSegment(segment)
+
+            return (
+              <React.Fragment key={i}>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  {isLast ? (
+                    <BreadcrumbPage>{label}</BreadcrumbPage>
+                  ) : (
+                    <BreadcrumbLink asChild>
+                      <Link href={href}>{label}</Link>
+                    </BreadcrumbLink>
+                  )}
+                </BreadcrumbItem>
+              </React.Fragment>
+            )
+          })}
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
   )
 }
