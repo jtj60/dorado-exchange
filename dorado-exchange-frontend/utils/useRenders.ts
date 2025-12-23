063fae82d206e5da3ff2f8923d8b98d945c41581
Jake Johnson <jaketjohnson97@gmail.com>
2025-12-22 22:22:38 -0600

    remove counter

--- dorado-exchange-frontend/utils/useRenders.ts
+++
@@ -1,8 +0,0 @@
-import { useRef } from "react"
-
-export function useRenderCount(label: string) {
-  const n = useRef(0)
-  n.current += 1
-  console.log(`[render] ${label} #${n.current}`)
-  return n.current
-}
